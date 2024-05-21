320ec465545a8b51e6f62a5458dfa913c17ea3bf
Enigmage <alizaidi0786.az@gmail.com>
2024-05-21 13:30:22 +0530

    hs512 decode

--- hs512/decode.py
+++ hs512/decode.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 import jwt
 
 load_dotenv()
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsaXphaWRpIiwidXNlcklEIjo5OTksImV4cCI6MTcxNjI3NzM3OX0.Yf0c3Msf7MQTCU2IHC5v5t1oxRsjrCX0CH2vDK-pT9g"
+token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsaXphaWRpIiwidXNlcklEIjo5OTksImV4cCI6MTcxNjI4MTczM30.ldTp3O22pEwwBWI-s3CpInSYXYalZ-cU_xfKp_R89HeRdyqgpz8_nnD8GB470gJvR8TKLKCGoArpL5TaLrZJhQ"
 
 secret = os.getenv("SECRET_KEY")
 
 
 try:
     if token and secret:
-        decoded = jwt.decode(token, secret, algorithms=["HS256"])
+        decoded = jwt.decode(token, secret, algorithms=["HS512"])
         print(decoded)
     else:
         print("Token or secret key not found.")
